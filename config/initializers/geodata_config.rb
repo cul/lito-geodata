bc7c49bec9f48845906afb0f651855a3880a61b8
Stuart Marquis <stuartmarquis@gmail.com>
2026-09-02 10:34:20 -0400

    minor cleanup

--- config/initializers/geodata_config.rb
+++ config/initializers/geodata_config.rb
@@ -4,5 +4,5 @@ APP_CONFIG ||= Rails.application.config_for(:app_config).to_h.with_indifferent_a
 # used on the /admin/system diagnostic page
 BOOTED_AT ||= Time.now
 LAST_DEPLOYED ||= File.atime(Dir.pwd).to_s
-GEODATA_VERSION ||= IO.read('VERSION').strip
+GEODATA_VERSION ||= Rails.root.join('VERSION').read.strip
 
